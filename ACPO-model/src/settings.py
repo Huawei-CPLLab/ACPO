904ea8c84bbd275d4f9b2af4449cecf62b76c11c
wangzm509 <786384629@qq.com>
2024-10-18 18:33:02 +0800

    [ACPO] train bug fix. Refs #2

--- ACPO-model/src/settings.py
+++ ACPO-model/src/settings.py
@@ -6,7 +6,6 @@ import yaml
 
 from losses import loss_dict
 
-
 def str2bool(v):
     if isinstance(v, bool):
        return v
@@ -50,7 +49,7 @@ parser.add_argument('--prune-value', type=list, default=None,
                     help="Threshold which we will use to prune data.")
 parser.add_argument('--unique-key', type=list, default=None, 
                     help="Unique key(s) to dataset which determine duplicates.")
-parser.add_argument('--class-key', type=list, default=None, 
+parser.add_argument('--class-key', type=list, default='Classes',
                     help="key(s)/feature(s) used to convert create classes.")
 parser.add_argument('--num-classes', type=int, default=1,
                     help="Number of possible classes which can be derived from the dataset itself.")
